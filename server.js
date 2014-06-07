c0cacce4c939f912b535b01c05d0bed7d3a9691d
Pixelistik <code@pixelistik.de>
2014-06-07 11:31:27 +0200

    Cleaning up, double quotes

--- server.js
+++ server.js
@@ -1,33 +1,33 @@
-var static = require('node-static');
-var clientFiles = new static.Server('./client');
+var static = require("node-static");
+var clientFiles = new static.Server("./client");
 
-var app = require('http').createServer(handler);
+var app = require("http").createServer(handler);
 
-var io = require('socket.io').listen(app);
+var io = require("socket.io").listen(app);
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port);
 
 function handler (request, response) {
-	request.addListener('end', function () {
+	request.addListener("end", function () {
 		clientFiles.serve(request, response);
 	}).resume();
 }
 
-io.sockets.on('connection', function (socket) {
+io.sockets.on("connection", function (socket) {
 	var group;
-	socket.on('join', function (data) {
+	socket.on("join", function (data) {
 		socket.join(data.groupName);
 
 		socket.userUuid = data.userUuid;
 
 		group = data.groupName;
-		socket.broadcast.to(group).emit('who is there');
+		socket.broadcast.to(group).emit("who is there");
 		console.log("Client joined group " + data.groupName);
 	});
 
-	socket.on('update', function (data) {
-		socket.broadcast.to(group).emit('update', data);
+	socket.on("update", function (data) {
+		socket.broadcast.to(group).emit("update", data);
 		console.log(data);
 	});
 

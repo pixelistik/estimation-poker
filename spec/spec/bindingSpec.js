5e13ce71c95a185ebc43cc4dab3a3211d202d48f
Pixelistik <code@pixelistik.de>
2014-12-04 21:46:14 +0100

    Removing old method of mocking a key event

--- spec/spec/bindingSpec.js
+++ spec/spec/bindingSpec.js
@@ -41,7 +41,7 @@ describe("Binding", function () {
 			$(".et-display", fixture).trigger("click");
 			$("input", fixture).val("entered value");
 
-			e = generateKeypressEvent(13); // ENTER key
+			e = crossBrowser_initKeyboardEvent("keypress", {key: "Enter", keyCode: 13});
 			fixture[0].querySelector("input").dispatchEvent(e);
 
 			expect(model.prop).toHaveBeenCalledWith("entered value");
@@ -63,7 +63,7 @@ describe("Binding", function () {
 			$(".et-display", fixture).trigger("click");
 			$("input", fixture).val("entered value");
 
-			e = generateKeypressEvent(27); //ESC key
+			e = crossBrowser_initKeyboardEvent("keypress", {key: "Esc", keyCode: 27});
 			fixture[0].querySelector("input").dispatchEvent(e);
 
 			expect(model.prop).not.toHaveBeenCalledWith("entered value");
@@ -137,40 +137,6 @@ describe("Binding", function () {
 			});
 		});
 	});
-
-	// Create a keypress event for testing
-	// see http://stackoverflow.com/a/23700583/376138
-	function generateKeypressEvent(code) {
-		var oEvent = document.createEvent('KeyboardEvent');
-
-		// Chromium Hack: filter this otherwise Safari will complain
-		if (navigator.userAgent.toLowerCase().indexOf('chrome') > -1) {
-			Object.defineProperty(oEvent, 'keyCode', {
-				get: function () {
-					return this.keyCodeVal;
-				}
-			});
-			Object.defineProperty(oEvent, 'which', {
-				get: function () {
-					return this.keyCodeVal;
-				}
-			});
-		}
-
-		if (oEvent.initKeyboardEvent) {
-			oEvent.initKeyboardEvent("keypress", true, true, document.defaultView, false, false, false, false, code, code);
-		} else {
-			oEvent.initKeyEvent("keypress", true, true, document.defaultView, false, false, false, false, code, 0);
-		}
-
-		oEvent.keyCodeVal = code;
-
-		if (oEvent.keyCode !== code) {
-			console.log("keyCode mismatch " + oEvent.keyCode + "(" + oEvent.which + ") -> " + code);
-		}
-
-		return oEvent;
-	}
 });
 
 // Create a keypress event for testing

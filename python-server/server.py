fb17b10dfb1b0b47caa2f5df30df71edc247f90e
Pixelistik <code@pixelistik.de>
2015-07-10 14:45:35 +0200

    Support for PRODUCTION_MODE env switch in Python
    In order to support loading the socket.io.js file, the route
    also had to be explicitly set in the JS server. The existing
    URL did not work, because the leading "socket.io" triggered
    some special handling in the Python middleware.

--- python-server/server.py
+++ python-server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, session
+from flask import Flask, render_template, send_from_directory, send_file, session
 from flask.ext.socketio import SocketIO, emit, join_room
 import os
 import pystache
@@ -9,14 +9,26 @@ app.config["SECRET_KEY"] = "b8weH7evX6ELU0Dy540zgrIt"
 app.debug = True
 socketio = SocketIO(app)
 
+indexData = {}
+
+if os.environ.has_key("PRODUCTION_MODE") and os.environ["PRODUCTION_MODE"] == "1":
+    indexData["productionMode"] = True
+else:
+    indexData["productionMode"] = False
+
 @app.route("/")
 def index():
     with open ("../client/index.html", "r") as indexfile:
         template = indexfile.read()
-    return pystache.render(template, {"productionMode": True})
+    return pystache.render(template, indexData)
+
+@app.route("/js/lib/socket.io.js")
+def send_socketio_js():
+    return send_file("../node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js")
 
 @app.route("/<path:path>")
-def send_js(path):
+def send_assets(path):
+    app.logger.debug("assetss")
     return send_from_directory("../client/", path)
 
 @socketio.on("join")

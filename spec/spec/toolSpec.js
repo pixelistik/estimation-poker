92d376fc5eaddb6e33a11ebcf8a085543bfdefce
Pixelistik <code@pixelistik.de>
2016-03-19 20:41:00 +0100

    Make Tool specs node module aware
    If the EP global does not exist, try to require the file.

--- spec/spec/toolSpec.js
+++ spec/spec/toolSpec.js
@@ -1,38 +1,40 @@
-describe("Tool", function () {
-	describe("Base64 ID", function () {
-		it("should return a string with the correct length", function () {
-			var result = EP.Tools.base64Id(6);
-			expect(result.length).toEqual(6);
+(function (EP) {
+	describe("Tool", function () {
+		describe("Base64 ID", function () {
+			it("should return a string with the correct length", function () {
+				var result = EP.Tools.base64Id(6);
+				expect(result.length).toEqual(6);
 
-			result = EP.Tools.base64Id(4);
-			expect(result.length).toEqual(4);
+				result = EP.Tools.base64Id(4);
+				expect(result.length).toEqual(4);
+			});
+
+			it("should return a string containing only a-z, A-Z, 0-9", function () {
+				var result = EP.Tools.base64Id(200);
+
+				expect(/[^a-zA-Z0-9]/.test(result)).toBeFalsy();
+			});
+
+			it("should use the first and last char, too (no off-by-one error)", function () {
+				var result = EP.Tools.base64Id(64000);
+
+				expect(/[a]/.test(result)).toBeTruthy();
+				expect(/[9]/.test(result)).toBeTruthy();
+			});
 		});
 
-		it("should return a string containing only a-z, A-Z, 0-9", function () {
-			var result = EP.Tools.base64Id(200);
+		describe("Safe mailto href", function () {
+			it("should contain the given subject and body", function () {
+				var result = EP.Tools.safeMailtoHref("theSubject", "theBody");
 
-			expect(/[^a-zA-Z0-9]/.test(result)).toBeFalsy();
-		});
+				expect(result).toBe("mailto:?subject=theSubject&body=theBody");
+			});
 
-		it("should use the first and last char, too (no off-by-one error)", function () {
-			var result = EP.Tools.base64Id(64000);
+			it("should not allow injection of additional parameters", function () {
+				var result = EP.Tools.safeMailtoHref("&bcc=evil@example.com", "&bcc=evil@example.com");
 
-			expect(/[a]/.test(result)).toBeTruthy();
-			expect(/[9]/.test(result)).toBeTruthy();
+				expect(result).not.toContain("&bcc=");
+			});
 		});
 	});
-
-	describe("Safe mailto href", function () {
-		it("should contain the given subject and body", function () {
-			var result = EP.Tools.safeMailtoHref("theSubject", "theBody");
-
-			expect(result).toBe("mailto:?subject=theSubject&body=theBody");
-		});
-
-		it("should not allow injection of additional parameters", function () {
-			var result = EP.Tools.safeMailtoHref("&bcc=evil@example.com", "&bcc=evil@example.com");
-
-			expect(result).not.toContain("&bcc=");
-		});
-	});
-});
+})(this.EP || { Tools: require("../../client/js/tools.js") });

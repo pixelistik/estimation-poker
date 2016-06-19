27c199dc5babc44f3ab74f9fea64a322429fe57b
Pixelistik <code@pixelistik.de>
2016-06-19 10:30:45 +0200

    Bundle module files with Browserify

--- client/js/app.js
+++ client/js/app.js
@@ -1,18 +1,29 @@
-(function(EP, ko, QRCode) {
-	"use strict";
-	if(location.hash === "") {
-		location.hash = EP.Tools.base64Id(8);
-	}
-	var poker = new EP.PokerView(location.hash);
-	ko.components.register('card', {
-		template: { element: 'card' }
-	});
-	ko.applyBindings(poker);
+"use strict";
 
-	new QRCode(document.getElementById("sharing-qr-code"), location + "");
+var ko = require("knockout");
+var io = require("socket.io-client");
 
-	EP.Tools.init();
+var Tools = require("./tools.js")
+var User = require("./models/User.js")(ko, Tools);
+var PokerView = require("./models/PokerView.js")(ko, Tools, User, io, window);
 
-	document.querySelector("body").classList.remove("uninitialised");
+if(location.hash === "") {
+	location.hash = Tools.base64Id(8);
+}
+var poker = new PokerView(location.hash);
+ko.components.register('card', {
+	template: { element: 'card' }
+});
 
-})(window.EP = window.EP || {}, window.ko, window.QRCode);
+require("./bindings/editableText.js")(ko);
+require("./bindings/estimationSelect.js")(ko, document);
+require("./bindings/pageTitle.js")(ko, document);
+
+ko.applyBindings(poker);
+
+// require("./lib/qrcode.js");
+// new QRCode(document.getElementById("sharing-qr-code"), location + "");
+
+Tools.init();
+
+document.querySelector("body").classList.remove("uninitialised");

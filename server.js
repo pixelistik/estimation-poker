4953315a0fad24b5eb47d1fcf71f578d3d497222
Pixelistik <code@pixelistik.de>
2022-09-12 14:25:45 +0200

    Update code formatting

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 "use strict";
 
 var express = require("express");
-var compression = require('compression');
-var mustacheExpress = require('mustache-express');
+var compression = require("compression");
+var mustacheExpress = require("mustache-express");
 var socketio = require("socket.io");
 
 const prometheusClient = require("prom-client");
@@ -22,18 +22,18 @@ app.set("views", __dirname + "/client");
 var packageInfo = require("./package.json");
 
 var indexData = {
-	packageInfo: packageInfo
+  packageInfo: packageInfo,
 };
 
 if (process.env.PIWIK_URL && process.env.PIWIK_SITE_ID) {
-	indexData.piwik = {
-		url: process.env.PIWIK_URL,
-		siteId: process.env.PIWIK_SITE_ID
-	};
+  indexData.piwik = {
+    url: process.env.PIWIK_URL,
+    siteId: process.env.PIWIK_SITE_ID,
+  };
 }
 
 if (process.env.PRODUCTION_MODE) {
-	indexData.productionMode = true;
+  indexData.productionMode = true;
 }
 
 const gauge = new prometheusClient.Gauge({
@@ -43,7 +43,7 @@ const gauge = new prometheusClient.Gauge({
 app.use(compression());
 
 app.get("/", function (request, response) {
-	response.render("index.html", indexData);
+  response.render("index.html", indexData);
 });
 
 app.use(express.static("./client", { maxAge: 1000 * 3600 * 24 * 365 }));
@@ -60,30 +60,30 @@ app.get("/metrics", async function (request, response) {
 });
 
 if (!process.env.PRODUCTION_MODE) {
-	// Automatic reloading for convenient dev environment
-	require("reload")(server, app, 1500);
+  // Automatic reloading for convenient dev environment
+  require("reload")(server, app, 1500);
 }
 
 io.sockets.on("connection", function (socket) {
-	var group;
-	socket.on("join", function (data) {
-		socket.join(data.groupName);
+  var group;
+  socket.on("join", function (data) {
+    socket.join(data.groupName);
 
-		socket.userUuid = data.userUuid;
+    socket.userUuid = data.userUuid;
 
-		group = data.groupName;
-		socket.broadcast.to(group).emit("who is there");
-	});
+    group = data.groupName;
+    socket.broadcast.to(group).emit("who is there");
+  });
 
-	socket.on("update", function (data) {
-		socket.broadcast.to(group).emit("update", data);
-	});
+  socket.on("update", function (data) {
+    socket.broadcast.to(group).emit("update", data);
+  });
 
-	socket.on("disconnect", function () {
-		socket.broadcast.to(group).emit("user disconnected", socket.userUuid);
-	});
+  socket.on("disconnect", function () {
+    socket.broadcast.to(group).emit("user disconnected", socket.userUuid);
+  });
 
-	socket.on("new round", function () {
-		socket.broadcast.to(group).emit("new round");
-	});
+  socket.on("new round", function () {
+    socket.broadcast.to(group).emit("new round");
+  });
 });
